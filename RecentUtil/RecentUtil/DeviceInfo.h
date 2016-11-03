9afcb7fc770c5a19ed5dbfe95b2f70347d14dacc
Lovecross <345841634@qq.com>
2016-11-03 15:46:20 +0800

    xx

---
+++ RecentUtil/RecentUtil/DeviceInfo.h
@@ -0,0 +1,13 @@
+//
+//  DeviceInfo.h
+//  RecentUtil
+//
+//  Created by huipay on 2016/11/3.
+//  Copyright © 2016年 huipay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeviceInfo : UIViewController
+
+@end
